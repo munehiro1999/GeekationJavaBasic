f32e551f4de284d013cf2c5af2dd6fc81730e08a
takahashimunehiro <takahashimunehiro@localhost>
2025-12-27 18:43:35 +0900

    Java基礎構文　変数課題

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,20 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int num1 = 100;
+		double num2 = 3.14;
+		
+		String myLike = "私の好きな";
+		String thisIs = "です。";
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		System.out.println(myLike + "整数は" + num1 + thisIs);
+		System.out.println(myLike + "少数は" + num2 + thisIs);
+		
+	}
+
+}
