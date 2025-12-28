2688aada28c97d2965c8f301588957f4d54d3833
takahashimunehiro <takahashimunehiro@localhost>
2025-12-28 22:54:47 +0900

    Java基礎構文　繰り返し処理課題　修正

--- src/basicSyntax/ForWhile.java
+++ src/basicSyntax/ForWhile.java
@@ -4,12 +4,15 @@ public class ForWhile {
 
 	public static void main(String[] args) {
 		for (int number = 1; number <= 100; number++) {
-			if (number % 3 == 0) {
+			if (number % 3 == 0 && number % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (number % 3 == 0) {
 				System.out.println("Fizz");
 			} else if (number % 5 == 0) {
 				System.out.println("Buzz");
-			} else if (number % 3 == 0 && number % 5 == 0);
-				System.out.println("FizzBuzz");
+			} else {
+				System.out.println(number);
+			}
 		}
 		
 		String[] programs = {"Ruby", "Golang", "Java", "Python", "Typescript", "HTML", "Javascript"};
@@ -23,6 +26,6 @@ public class ForWhile {
 				break;
 		}
 			System.out.println(program);
-	}
+		}
 	}
 }
